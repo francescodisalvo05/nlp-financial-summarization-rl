eb1b2263b1515ef118a025925a8cbfa21d0e4404
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-06-27 21:42:15 +0200

    Move and fix utils

--- utils/utils.py
+++ utils/utils.py
@@ -21,22 +21,24 @@ PAD = 0
 UNK = 1
 START = 2
 END = 3
-def make_vocab(wc, vocab_size):
+def make_vocab(wc):
     word2id, id2word = {}, {}
     word2id['<pad>'] = PAD
     word2id['<unk>'] = UNK
-    word2id['<start>'] = START
-    word2id['<end>'] = END
-    for i, (w, _) in enumerate(wc.most_common(vocab_size), 4):
-        word2id[w] = i
+    word2id['<SOS>'] = START
+    word2id['<END>'] = END
+    for i, w in enumerate(wc, 4):
+        # avoid duplicates -> they are on the vocab
+        if w != '<SOS>' and w!='<EOS>':
+            word2id[w] = i
     return word2id
 
 
-def make_embedding(id2word, w2v_file, initializer=None):
-    attrs = basename(w2v_file).split('.')  #word2vec.{dim}d.{vsize}k.bin
-    w2v = gensim.models.Word2Vec.load(w2v_file).wv
+def make_embedding(id2word, w2v, initializer=None):
+    #attrs = basename(w2v_file).split('.')  #word2vec.{dim}d.{vsize}k.bin
+    #w2v = gensim.models.Word2Vec.load(w2v_file).wv
     vocab_size = len(id2word)
-    emb_dim = int(attrs[-3][:-1])
+    emb_dim = 300#int(attrs[-3][:-1])
     embedding = nn.Embedding(vocab_size, emb_dim).weight
     if initializer is not None:
         initializer(embedding)
@@ -46,9 +48,9 @@ def make_embedding(id2word, w2v_file, initializer=None):
         for i in range(len(id2word)):
             # NOTE: id2word can be list or dict
             if i == START:
-                embedding[i, :] = torch.Tensor(w2v['<s>'])
+                embedding[i, :] = torch.Tensor(w2v['<SOS>'])
             elif i == END:
-                embedding[i, :] = torch.Tensor(w2v[r'<\s>'])
+                embedding[i, :] = torch.Tensor(w2v['<EOS>'])
             elif id2word[i] in w2v:
                 embedding[i, :] = torch.Tensor(w2v[id2word[i]])
             else:
