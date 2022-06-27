9700cde31c31c1779ab850451f7e6c4e11e40d5c
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-06-27 19:43:01 +0200

    Add data preprocessing step

--- preprocess_text.py
+++ preprocess_text.py
@@ -1,4 +1,4 @@
-from utils.preprocessing import clean_pipeline
+from utils.preprocessing import clean_pipeline, filter_pipeline
 
 import argparse
 import logging
@@ -13,30 +13,42 @@ def main(args):
     nlp = spacy.load("en_core_web_sm")
     nlp.max_length = 150000
 
-    # # # set directories and preprocess the text
-    train_flag = 'train' in args.dataset_split
-    val_flag = 'val' in args.dataset_split
-    test_flag = 'test' in args.dataset_split
 
     if not os.path.exists(args.preprocessed_path):
         os.makedirs(args.preprocessed_path)
 
+    if not os.path.exists(args.filtered_path):
+        os.makedirs(args.filtered_path)
 
-    if train_flag:
-        clean_pipeline(args.dataset_path, args.reports_folder,
-                       args.summaries_folder, args.preprocessed_path,
-                       'full_corpus.txt', 'train',nlp)
+    # train
+    clean_pipeline(args.dataset_path, args.reports_folder,
+                   args.summaries_folder, args.preprocessed_path,
+                   'full_corpus.txt', 'train', nlp)
 
-    if val_flag:
-        clean_pipeline(args.dataset_path, args.reports_folder,
-                       args.summaries_folder, args.preprocessed_path,
-                       None,'val',nlp)
+    filter_pipeline(args.preprocessed_path, args.reports_folder,
+                       args.summaries_folder, args.filtered_path,
+                       'full_corpus.txt', 'full_corpus_filtered.txt',
+                       'tokens_to_remove.json', 'train')
 
-    if test_flag:
-        clean_pipeline(args.dataset_path, args.reports_folder,
-                       args.summaries_folder, args.preprocessed_path,
-                       None, 'test', nlp)
+    # validation
+    clean_pipeline(args.dataset_path, args.reports_folder,
+                   args.summaries_folder, args.preprocessed_path,
+                   None, 'val', nlp)
 
+    filter_pipeline( args.preprocessed_path, args.reports_folder,
+                     args.summaries_folder, args.filtered_path,
+                     'full_corpus.txt', 'full_corpus_filtered.txt',
+                     'tokens_to_remove.json', 'val')
+
+    # test
+    clean_pipeline(args.dataset_path, args.reports_folder,
+                   args.summaries_folder, args.preprocessed_path,
+                   None, 'test', nlp)
+
+    filter_pipeline(args.preprocessed_path, args.reports_folder,
+                        args.summaries_folder, args.filtered_path,
+                        'full_corpus.txt', 'full_corpus_filtered.txt',
+                        'tokens_to_remove.json', 'test')
 
 
 if __name__ == "__main__":
@@ -45,9 +57,8 @@ if __name__ == "__main__":
 
     parser.add_argument('-D', '--dataset_path', type=str, required=True, help='Set the base dataset path')
     parser.add_argument('-P', '--preprocessed_path', type=str, required=True, help='Set the base dataset path for the preprocessed files')
-
-    parser.add_argument('-d', '--dataset_split', nargs='+', required=True, help='Set the dataset split to preprocess (even more than one)',
-                        choices=['train','val','test'])
+    parser.add_argument('-F', '--filtered_path', type=str, required=True,
+                        help='Set the base dataset path for the filtered files')
 
     parser.add_argument('-r', '--reports_folder', type=str, required=True, help='Name of the reports folder')
     parser.add_argument('-s', '--summaries_folder', type=str, required=True, help='Name of the summaries folder')
@@ -57,3 +68,4 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
 
     main(args)
+
